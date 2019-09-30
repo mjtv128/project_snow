6a41e97602096e01b2cfe981d4a40a6c62956129
ck2e14 <chriswkennedy@icloud.com>
2019-09-30 19:05:57 +0100

    built controller methods, and review views

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ActiveRecord::Base 
     belongs_to :user 
     belongs_to :resort
+
+    validates :rating, numericality: {less_than_or_equal_to: 10}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :users
+  resources :reviews
+  resources :resorts, only: [:index, :show]
+  resources :conversation, only: [:create, :destroy, :show]
+  resources :messages
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
 end
