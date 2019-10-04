5a21044c6e545f8699e7561a111dd11b05fd09e1
ck2e14 <chriswkennedy@icloud.com>
2019-10-04 11:52:02 +0100

    added search function for resorts

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,6 +1,6 @@
 class MeetupsController < ApplicationController
     def index
-        @meetups = Meetup.all
+        @meetups = Meetup.search(params[:search])
     end
 
     def new 
--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -3,4 +3,18 @@ class Meetup < ActiveRecord::Base
     has_many :attendees_meetups
     belongs_to :organiser, :class_name => "User", :foreign_key => :organiser_id
     has_many :attendees, :class_name => "User", through: :attendees_meetups
+
+     def self.search(search)
+        if search 
+            resort = Resort.find_by(resort_name: search)
+            if resort
+                self.where(resort_id: resort)
+            else
+                Meetup.all
+            end
+        else
+            Meetup.all
+        
+        end
+    end
 end
