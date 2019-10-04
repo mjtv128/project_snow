ee5424e3ae12ffbbacb3413f87cfddfd2100dee0
ck2e14 <chriswkennedy@icloud.com>
2019-10-04 11:36:42 +0100

    added 'my meetups' to profile page

--- app/controllers/attendees_meetups_controller.rb
+++ app/controllers/attendees_meetups_controller.rb
@@ -6,6 +6,7 @@ class AttendeesMeetupsController < ApplicationController
     def create
         @attendees_meetup = AttendeesMeetup.create(attendee_id: session[:user_id], meetup_id: params[:id])
         redirect_to meetup_path(params[:id])
+        flash[:notice] = "You have been marked as attending!"
         # byebug
     end
 
